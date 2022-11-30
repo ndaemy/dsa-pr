8a9d389dfc6a072a1db1d945791afd9d5aaf831f
ndaemy <yuyaebean@gmail.com>
2022-12-01 05:27:50 +0900

    📝 Set title and subtitle

---
+++ src/components/Main.tsx
@@ -0,0 +1,12 @@
+import type { FC } from "react";
+
+const Main: FC = () => {
+  return (
+    <div className="px-4 py-24">
+      <h1 className="text-4xl text-center font-bold leading-relaxed mb-3">고등부 학생처</h1>
+      <h3 className="text-lg text-center text-neutral-400 font-normal leading-normal mb-2">고등부 학생처는 7개 부서로 이루어져 있습니다.</h3>
+    </div>
+  );
+}
+
+export default Main;
