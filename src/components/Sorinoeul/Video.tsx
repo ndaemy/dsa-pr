0418d36fd53d96167163c54504872cbd57fbf45b
ndaemy <yuyaebean@gmail.com>
2023-04-22 23:02:48 +0900

    ✨ 비디오 자동재생 업데이트 및 무음처리

--- src/components/Sorinoeul/Video.tsx
+++ src/components/Sorinoeul/Video.tsx
@@ -1,9 +1,7 @@
 export const Video = () => {
   return (
-    <video
-      src="https://ndaemy-static.s3.ap-northeast-2.amazonaws.com/fgcs/230422_sorinoeul.mp4"
-      autoPlay
-      controls
-    />
+    <video autoPlay controls muted>
+      <source src="https://ndaemy-static.s3.ap-northeast-2.amazonaws.com/fgcs/230422_sorinoeul.mp4" />
+    </video>
   );
 };
